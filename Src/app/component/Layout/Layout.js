36267322804c1472dc251b752757e8841d47a4c4
Gunjan <gunjanbothra24@gmail.com>
2022-03-08 11:20:45 +0530

    added Corporate gift page

--- Src/app/component/Layout/Layout.js
+++ Src/app/component/Layout/Layout.js
@@ -1,14 +1,27 @@
 import styles from "./Layout.module.css";
+import Image from "next/image";
 import OccassionMenu from "../../component/OccassionMenu/OccassionMenu";
 
 const Layout = ({ children }) => {
   return (
-    <div className={styles.page}>
-      <section className={styles.leftSection}>
-        {/* <OccassionMenu></OccassionMenu> */}
-      </section>
-      <section className={styles.container}>{children}</section>
-      <section className={styles.rightSection}></section>
+    <div>
+      <header>
+        <div className={styles.headerLogo}>
+          <Image
+            src="/images/logo.jpeg" // Route of the image file
+            height={44} // Desired size with correct aspect ratio
+            width={68} // Desired size with correct aspect ratio
+            alt="Logo"
+          />
+        </div>
+      </header>
+      <div className={styles.page}>
+        <section className={styles.leftSection}>
+          {/* <OccassionMenu></OccassionMenu> */}
+        </section>
+        <section className={styles.container}>{children}</section>
+        <section className={styles.rightSection}></section>
+      </div>
     </div>
   );
 };
